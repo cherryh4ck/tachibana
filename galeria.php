9c0d528563717086f52b7301e4015ee926724abc
tomas <tomas1234567891016@gmail.com>
2025-07-13 21:43:19 -0300

    Cambiar título

--- galeria.php
+++ galeria.php
@@ -26,7 +26,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subir</title>
+    <title>Galería</title>
     <link rel="stylesheet" href="styles/styles.css">
     <script src="js/archivos.js" defer></script>
     <script src="js/tags.js" defer></script>
