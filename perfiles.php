0b2a7b6b4d6c172c99b6488b02ed8ce710a77ca5
tomas <tomas1234567891016@gmail.com>
2025-07-15 11:48:46 -0300

    perfiles.php

---
+++ perfiles.php
@@ -0,0 +1,58 @@
+<?php
+    // por hacer: todooooo
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="js/register/verify.js" defer></script>
+</head>
+<body>
+    <nav>
+        <h1>test</h1>
+        <ul>
+            <li><a href="galeria.php?pag=1">Galería</a></li>
+            <li><a href="subir.php">Subir</a></li>
+            <li><a href="perfiles.php">Usuarios</a></li>
+        </ul>
+        <div class="nav-cuenta">
+            <a href="php/cuenta.php" id="cuenta">Invitado</a>
+        </div>
+    </nav>
+    <div class="contenido-perfiles">
+        <p>Buscar un usuario</p>
+        <input type="text" name="nombreUsuario" placeholder="Nombre de usuario..." id="nombreUsuario">
+    </div>
+    <div class="contenido-perfiles-usuarios">
+        <p>Usuarios</p>
+        <div class="contenido-perfiles-usuarios-lista">
+            <div class="contenido-perfil-bloque">
+                <img src="resources/avatar.png" alt="">
+                <div class="contenido-perfil-bloque-info">
+                    <p><b>Usuario</b></p>
+                    <p>Pequeña biografía dalfslalkfkalkflkalkfalklkslkf (recortar texto).</p>
+                </div>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
