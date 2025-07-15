802782b94c7d1750b85236d186e17312a06ea22c
tomas <tomas1234567891016@gmail.com>
2025-07-15 11:49:17 -0300

    perfiles.php

--- perfiles.php
+++ perfiles.php
@@ -1,5 +1,6 @@
 <?php
     // por hacer: todooooo
+    // falta bastante
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuarios</title>
     <link rel="stylesheet" href="styles/styles.css">
-    <script src="js/register/verify.js" defer></script>
 </head>
 <body>
     <nav>
