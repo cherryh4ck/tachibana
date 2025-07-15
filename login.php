9fec89b528cf4ff7f415c3cb13346d24e095b1c6
tomas <tomas1234567891016@gmail.com>
2025-07-14 21:26:09 -0300

    Añadir formulario

--- login.php
+++ login.php
@@ -5,3 +5,35 @@
         header("Location: galeria.php");
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <nav>
+        <h1>test</h1>
+        <ul>
+            <li><a href="galeria.php?pag=1">Galería</a></li>
+            <li><a href="subir.php">Subir</a></li>
+            <li><a href="perfiles.php">Usuarios</a></li>
+        </ul>
+        <div class="nav-cuenta">
+            <a href="php/cuenta.php" id="cuenta">Invitado</a>
+        </div>
+    </nav>
+    <div class="contenido-menu">
+        <form action="php/login.php" method="post">
+            <p>Iniciar sesión</p>
+            <input type="text" name="user" placeholder="Nombre de usuario" required>
+            <input type="password" name="password" placeholder="Contraseña" required>
+            <input type="submit" value="Iniciar sesión">
+        </form>
+        <p id="registrate">¿No tenés cuenta? Registrate <a href="register.php">acá</a></p>
+    </div>
+</body>
+</html>
