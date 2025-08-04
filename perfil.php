d3b749eac186bbed37c25a78b32110a0c416903d
tomas <tomas1234567891016@gmail.com>
2025-08-04 10:56:38 -0300

    Pequeño avance de como va a ser (falta muchísimo)

--- perfil.php
+++ perfil.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
     if (isset($_GET["id"])){
-        // Mostrar cuenta
+        // Mostrar cuenta si es que existe
+        $nombreDeUsuario = "Default";
     }
     else{
         if (isset($_SESSION["logueado"])){
@@ -12,3 +13,37 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+        echo "<title>" . $nombreDeUsuario . "</title>";
+    ?>
+    <link rel="stylesheet" href="styles/styles.css">
+    <script src="js/archivos.js" defer></script>
+    <link rel="shortcut icon" href="favicon.ico" />
+</head>
+<body>
+    <nav>
+        <h1>test</h1>
+        <ul>
+            <li><a href="galeria.php?pag=1">Galería</a></li>
+            <li><a href="subir.php">Subir</a></li>
+            <li><a href="perfiles.php">Usuarios</a></li>
+        </ul>
+        <div class="nav-cuenta">
+            <a href="php/cuenta.php" id="cuenta">Invitado</a>
+        </div>
+    </nav>
+    <header>
+        <div class="perfil-div">
+            <div class="perfil-banner">
+                <p>User</p>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
