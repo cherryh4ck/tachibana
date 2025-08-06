33125b13213506a1144d76b1cf7649b17b6dc8a0
tomas <tomas1234567891016@gmail.com>
2025-08-06 00:42:34 -0300

    Cambiar nav

--- perfiles.php
+++ perfiles.php
@@ -1,27 +1,6 @@
 <?php
-    if (isset($_GET["id"]) && is_numeric($_GET["id"])){
-        $id = $_GET["id"];
-        if (!file_exists("galeria/fullsize/" . $id . ".jpg")){
-            if (file_exists("galeria/fullsize/" . $id . ".gif")){
-                $ext = "gif";
-            }
-            else{
-                header("Location: error.php?id=2");
-                exit();
-            }
-        }
-        else{
-            $ext = "jpg";
-        }
-    }
-    else{
-        header("Location: error.php?id=2");
-        exit();
-    }
-
-    $post_titulo = "Sin nombre"; // se debe conseguir a través de una base de datos
-
-    //todo: mejores tags - like y dislikes - mejor preview (?)
+    // por hacer: todooooo
+    // falta bastante
 ?>
 
 <!DOCTYPE html>
@@ -29,11 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php
-        echo "<title>" . $post_titulo . "</title>";
-    ?>
+    <title>Usuarios</title>
     <link rel="stylesheet" href="styles/styles.css">
-    <script src="js/post/comentar.js" defer></script>
     <link rel="shortcut icon" href="favicon.ico" />
 </head>
 <body>
@@ -48,60 +24,37 @@
             <a href="php/cuenta.php" id="cuenta">Anónimo</a>
         </div>
     </nav>
-    <header>
-        <div class="contenido-post">
-            <?php
-                echo "<img src='galeria/fullsize/" . $id . "." . $ext ."'>";
-                echo "<div class='post-contenido'>";
-                echo "<div class='post-contenido-titulo'>";
-                echo "<h1 id='post-titulo'>Título</h1>";
-                echo "<h2 id='post-id'>ID #" . $id . "</h2>";
-                echo "<div class='post-contenido-tags'>";
-                echo "<span id='input-tag2'>tag1</span>";
-                echo "<span id='input-tag2'>tag2</span>";
-                echo "<span id='input-tag2'>asfsafafafafagg</span>";
-                echo "<span id='input-tag2'>test tes test t eststst</span>";
-                echo "<span id='input-tag2'>test tes test t eststst</span>";
-                echo "<span id='input-tag2'>test tes test t eststst</span>";
-                echo "<span id='input-tag2'>test tes test t eststst</span>";
-                echo "<span id='input-tag2'>test tes test t eststst</span>";
-                echo "</div>";
-                echo "<div class='post-autor'>";
-                echo "<img src='resources/avatar.png' alt='Avatar' id='post-autor-avatar'>";
-                echo "<div class='post-autor-info'>";
-                echo "<p><b>Autor</b><br>";
-                echo "<p>Publicado el 24-07-2025 a las 5:04 AM</p>";
-                echo "</div>";
-                echo "</div>";
-                echo "<h2 id='post-titulo-descripcion'>Descripción</h2>";
-                echo "<p id='post-descripcion'>Descripción. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium aspernatur alias harum velit laborum perspiciatis quisquam autem voluptatum ducimus deleniti. Placeat fugiat veniam provident, blanditiis quos voluptatum aliquam cupiditate ullam.</p>";
-                echo "</div>";
-            ?>
-            <h2 id="post-comentarios-titulo">Comentarios</h1>
-            <div class="post-comentarios">
-                <div class="post-comentarios-comentario">
-                    <img src="resources/avatar.png" alt="" id="post-comentarios-comentario-avatar">
-                    <p><b>User</b><br>Hola</p>
-                </div>
-                <div class="post-comentarios-comentario">
-                    <img src="resources/avatar.png" alt="" id="post-comentarios-comentario-avatar">
-                    <p><b>User</b><br>Testeo</p>
-                </div>
-                <div class="post-comentarios-comentario">
-                    <img src="resources/avatar.png" alt="" id="post-comentarios-comentario-avatar">
-                    <p><b>User</b><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae accusamus quidem voluptatum eius. In sunt numquam sequi error vel fugit, consequatur, nesciunt temporibus commodi corporis reiciendis, excepturi sapiente. Facere, debitis.</p>
+    <div class="contenido-perfiles">
+        <p>Buscar un usuario</p>
+        <input type="text" name="nombreUsuario" placeholder="Nombre de usuario..." id="nombreUsuario">
+    </div>
+    <div class="contenido-perfiles-usuarios">
+        <p>Usuarios</p>
+        <div class="contenido-perfiles-usuarios-lista">
+            <div class="contenido-perfil-bloque" onclick="location.href='perfil.php?id=1'">
+                <img src="resources/avatar.png" alt="">
+                <div class="contenido-perfil-bloque-info">
+                    <p><b>Display name</b></p>
+                    <p id="contenido-perfil-bloque-info-username">@usuario</p>
+                    <p>Pequeña biografía dalfslalkfkalkflkalkfalklkslkf</p>
                 </div>
             </div>
-            <h2 id="post-comentarios-comentar">Comentar</h2>
-            <div class="post-comentarios-comentar">
-                <img src="resources/avatar.png" alt="" id="post-comentarios-comentario-avatar">
-                <form action="" method="post">
-                    <textarea name="" id="post-comentarios-textarea" rows="2" required></textarea>
-                    <input type="submit" value="Comentar" id="post-comentarios-enviar" disabled>
-                </form>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
             </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
+            </div>
+            <div class="contenido-perfil-bloque">
+                <p>jejejejej</p>
             </div>
         </div>
-    </header>
+    </div>
 </body>
 </html>
