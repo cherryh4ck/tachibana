4ee640125587c59c319fbbc5d3c04fbdac4cd3c1
cherry <tomas1234567891016@gmail.com>
2025-08-09 19:03:09 -0300

    Añadir posibilidad de remover tags

--- js/tags.js
+++ js/tags.js
@@ -1,5 +1,3 @@
-// por hacer: mensaje de error - borrar tags
-
 let caracteres_permitidos = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_ ";
 let tag_permitido = true;
 let tags = [];
@@ -12,7 +10,7 @@ let tags_input = document.getElementById("tags-input");
 let titulo_input = document.getElementById("titulo-input")
 let textarea = document.getElementById("descripcion-input");
 let enviar = document.getElementById("btn-enviar");
-// let mensaje_error = document.getElementById("error-msj");
+let mensaje_error = document.getElementById("mensaje-error");
 
 let formulario = document.getElementById("formulario-subir");
 
@@ -29,10 +27,26 @@ function introducirTag(tag){
         if (tag_permitido){
             tags_introducidos++;
             tags.push(tag)
+
+            mensaje_error.style.display = "none";
+
             let span = document.createElement("span");
             span.className = "input-tag";
             span.id = "input-tag";
             span.innerText = tag;
+
+            let botonRemover = document.createElement("input");
+            botonRemover.id = "remover-tag";
+            botonRemover.type = "button";
+            botonRemover.value = "x";
+            botonRemover.addEventListener("click", function () {
+                tags_introducidos--;
+                tags = tags.filter(t => t !== tag);
+                span.remove();
+            });
+
+            span.appendChild(botonRemover);
+
             insert_tags.appendChild(span);
             if (req1 && req2 && req3 && titulo_input.value.trim() != "" && textarea.value.trim() != ""){
                 enviar.disabled = false;
@@ -42,11 +56,24 @@ function introducirTag(tag){
             }
         }
         else{
-            console.log("Tag no permitido");
+            mensaje_error.innerHTML = "<span>Error al añadir tag: </span> El tag contiene caracteres ilegales";
+            mensaje_error.style.display = "block";
         }
     }
     else{
-        console.log("Tag demasiado largo, vacío o ya hay 4 tags introducidos");
+        if (tag.length > 20){
+            mensaje_error.innerHTML = "<span>Error al añadir tag: </span> El tag supera los 20 caracteres";
+        }
+        else if (tags_introducidos >= 4){
+            mensaje_error.innerHTML = "<span>Error al añadir tag: </span> El máximo de tags es de 4";
+        }
+        else if (tag.length < 4){
+            mensaje_error.innerHTML = "<span>Error al añadir tag: </span> El tag debe tener al menos 4 caracteres";
+        }
+        else{
+            mensaje_error.innerHTML = "<span>Error al añadir tag: </span> El tag no puede estar vacío";
+        }
+        mensaje_error.style.display = "block";
     }
 }
 
