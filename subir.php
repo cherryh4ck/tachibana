73d60751ec0d8e3636945c7045a123aa79a21b7e
cherry <tomas1234567891016@gmail.com>
2025-08-11 00:09:10 -0300

    Modificar placeholder (añadir punto)

--- subir.php
+++ subir.php
@@ -65,7 +65,7 @@
                         <div class="contenido-subir-formulario-fila1-input">
                             <p>Tags</p>
                             <div class="contenido-subir-formulario-fila1-input-tags" id="insert-tags">
-                                <p id="no-hay-tags">Añade un tag</p>
+                                <p id="no-hay-tags">Añade un tag...</p>
                             </div>
                         </div>
                         <div class="contenido-subir-formulario-fila1-input">
