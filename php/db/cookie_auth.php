45772335ca97b38b166a2789d6e50801cc4294cc
cherry <tomas1234567891016@gmail.com>
2025-08-23 20:47:06 -0300

    Modificar nombres de las variables para no intervenir

--- php/db/cookie_auth.php
+++ php/db/cookie_auth.php
@@ -2,15 +2,15 @@
     if (isset($_COOKIE["auth"]) && !isset($_SESSION["cuenta_id"])){
         $auth = $_COOKIE["auth"];
         try{
-            $conn = new PDO("mysql:host=$host:$puerto;dbname=$db", $user, $pass);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn_auth = new PDO("mysql:host=$host:$puerto;dbname=$db", $user, $pass);
+            $conn_auth->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sql = $conn->prepare("SELECT * FROM usuarios WHERE auth_cookie = ?");
-            $sql->execute([$auth]);
-            $fetch = $sql->fetch(PDO::FETCH_ASSOC);
-            if ($fetch){
-                $_SESSION["cuenta_id"] = $fetch["id"];
-                $_SESSION["cuenta_usuario"] = $fetch["username"];
+            $sql_auth = $conn_auth->prepare("SELECT * FROM usuarios WHERE auth_cookie = ?");
+            $sql_auth->execute([$auth]);
+            $fetch_auth = $sql_auth->fetch(PDO::FETCH_ASSOC);
+            if ($fetch_auth){
+                $_SESSION["cuenta_id"] = $fetch_auth["id"];
+                $_SESSION["cuenta_usuario"] = $fetch_auth["username"];
             }
             else{
                 // simplemente eliminamos el cookie auth para prevenir más solicitudes
