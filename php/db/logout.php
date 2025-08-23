a4ed5d481b56b8f24aa9e7dff3b02ed2f0013301
cherry <tomas1234567891016@gmail.com>
2025-08-23 19:56:34 -0300

    Remover cookie de autenticación de la base de datos

--- php/db/logout.php
+++ php/db/logout.php
@@ -1,9 +1,22 @@
 <?php
     session_start();
-    session_unset();
-    session_destroy();
-    unset($_COOKIE["auth"]);
-    setcookie("auth", ""); 
-    
+    require "config.php";
+
+    if (isset($_SESSION["cuenta_id"])){
+        try{
+            $conn = new PDO("mysql:host=$host:$puerto;dbname=$db", $user, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = $conn->prepare("UPDATE usuarios SET auth_cookie = ? WHERE id = ?");
+            $sql->execute([NULL, $_SESSION["cuenta_id"]]);
+        }
+        catch (PDOException $e){
+            header("Location: ../../error.php?id=9");
+        }
+        session_unset();
+        session_destroy();
+        unset($_COOKIE["auth"]);
+        setcookie("auth", ""); 
+    }
     header("Location: ../../index.php");
 ?>
