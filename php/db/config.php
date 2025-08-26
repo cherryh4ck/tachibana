652be1d0e33a0cefffcd8cac474c8a538bc4382b
cherry <tomas1234567891016@gmail.com>
2025-08-26 18:30:50 -0300

    Hacer el config más dinámico

--- php/db/config.php
+++ php/db/config.php
@@ -1,9 +1,15 @@
 <?php
-    $host = "localhost";
-    $puerto = 3306;
-    $user = "root";
-    $pass = "";
-    $db = "tachibana";
+    $configuracion_ini = parse_ini_file(__DIR__ . "\config.ini", true);
+
+    // posts
+    $chequeo_estricto_imagen = $configuracion_ini["posts"]["chequeo_estricto_imagen"];
+
+    // bd
+    $host = $configuracion_ini["network"]["host"];
+    $puerto = $configuracion_ini["network"]["puerto"];
+    $user = $configuracion_ini["network"]["username"];
+    $pass = $configuracion_ini["network"]["password"];
+    $db = $configuracion_ini["network"]["database"];
 
     require "cookie_auth.php";
 ?>
