c3fd985968dd6853fe6fdd3603cd28f78eddeff1
cherry <tomas1234567891016@gmail.com>
2025-08-29 22:53:03 -0300

    Prevenir nicknames muy cortos o muy largos de manera clientside

--- js/perfil/editar.js
+++ js/perfil/editar.js
@@ -2,9 +2,16 @@ const avatar_input = document.getElementById("avatar-file");
 const avatar_preview = document.getElementById("avatar-img");
 
 const imagen_estirada_aviso = document.getElementById("mensaje-aviso");
+const mensaje_error = document.getElementById("mensaje-error");
 
 imagen_estirada_aviso.style.transition = "opacity 0.3s ease";
 imagen_estirada_aviso.style.opacity = 0;
+
+mensaje_error.style.transition = "opacity 0.3s ease";
+mensaje_error.style.opacity = 0;
+
+const formulario = document.getElementById("formulario-editar-perfil");
+const nickname_input = document.getElementById("nickname-input");
 
 avatar_input.addEventListener("change", (event) => {
     const imagen = event.target.files[0];
@@ -21,6 +28,7 @@ avatar_input.addEventListener("change", (event) => {
             });
         } else {
             imagen_estirada_aviso.style.opacity = 0;
+            mensaje_error.style.opacity = 0;
         }
     };
 });
@@ -29,4 +37,22 @@ imagen_estirada_aviso.addEventListener('transitionend', e=>{
   if (e.propertyName === 'opacity' && getComputedStyle(imagen_estirada_aviso).opacity === "0") {
     imagen_estirada_aviso.style.display = "none";
   }
+});
+
+mensaje_error.addEventListener('transitionend', e=>{
+  if (e.propertyName === 'opacity' && getComputedStyle(mensaje_error).opacity === "0") {
+    mensaje_error.style.display = "none";
+  }
+});
+
+formulario.addEventListener("submit", function(e) {
+    if ((nickname_input.value.length < 3) || (nickname_input.value.length > 19)){
+        e.preventDefault();
+        mensaje_error.innerHTML = "<span>Error al editar el perfil:</span> El nickname es muy corto o largo";
+        mensaje_error.style.opacity = 0;
+        mensaje_error.style.display = "block";
+        requestAnimationFrame(() => {
+            mensaje_error.style.opacity = 1; 
+        });
+    }
 });
