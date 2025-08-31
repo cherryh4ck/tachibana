dd3fcea079eca6137be2f73fd5afb5456dc02082
cherry <tomas1234567891016@gmail.com>
2025-08-31 03:06:31 -0300

    Pequeña optimización

--- php/db/cookie_auth.php
+++ php/db/cookie_auth.php
@@ -2,10 +2,7 @@
     if (isset($_COOKIE["auth"]) && !isset($_SESSION["cuenta_id"])){
         $auth = $_COOKIE["auth"];
         try{
-            $conn_auth = new PDO("mysql:host=$host:$puerto;dbname=$db", $user, $pass);
-            $conn_auth->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $sql_auth = $conn_auth->prepare("SELECT * FROM usuarios WHERE auth_cookie = ?");
+            $sql_auth = $conn->prepare("SELECT * FROM usuarios WHERE auth_cookie = ?");
             $sql_auth->execute([$auth]);
             $fetch_auth = $sql_auth->fetch(PDO::FETCH_ASSOC);
             if ($fetch_auth){
