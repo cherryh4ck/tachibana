0803a95cc72ab05489483f49360df9ccfe376cbc
cherry <tomas1234567891016@gmail.com>
2025-09-01 19:21:35 -0300

    Añadir animaciones

--- js/register/verify.js
+++ js/register/verify.js
@@ -8,27 +8,36 @@ const mensaje = document.createElement("p");
 mensaje.id = "formulario-mensaje";
 mensaje.innerHTML = "<span>Error:</span> El usuario ya existe.";
 
+mensaje.style.transition = "opacity 0.3s ease";
+mensaje.style.opacity = 0;
+
+let error = false;
+
 formulario.addEventListener("submit", function(event) {
     divMensaje.innerHTML = "";
 
     if (contraseña.value !== repetirContraseña.value){
         event.preventDefault();
         mensaje.innerHTML = "<span>Error:</span> Las contraseñas no coinciden";
+        error = true;
     }
     else{
         if (contraseña.value == usernameF.value){
             event.preventDefault();
             mensaje.innerHTML = "<span>Error:</span> La contraseña no puede ser igual al usuario";
+            error = true;
         }
         else{
             if ((username.value.length < 4) || (username.value.length > 19)){
                 event.preventDefault();
                 mensaje.innerHTML = "<span>Error:</span> El usuario es muy corto o es muy largo";
+                error = true;
             }
             else{
                 if (contraseña.value.length < 6){
                     event.preventDefault();
                     mensaje.innerHTML = "<span>Error:</span> La contraseña debe tener al menos 6 caracteres";
+                    error = true;
                 }
                 else{
                     mensaje.innerHTML = "Creando cuenta...";
@@ -38,4 +47,17 @@ formulario.addEventListener("submit", function(event) {
     }
 
     divMensaje.append(mensaje);
+    if (error == true){
+        mensaje.style.opacity = 0;
+        mensaje.style.display = "block";
+        requestAnimationFrame(() => {
+            mensaje.style.opacity = 1; 
+        });
+    }
+});
+
+mensaje.addEventListener('transitionend', e=>{
+    if (e.propertyName === 'opacity' && getComputedStyle(mensaje).opacity === "0") {
+      mensaje.style.display = "none";
+    }
 });
