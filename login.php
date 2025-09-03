a49461ef47d7ca865aaeb95404974a15c288c24c
cherry <tomas1234567891016@gmail.com>
2025-09-02 21:28:58 -0300

    Redirigir al login si es que tuvo éxito

--- login.php
+++ login.php
@@ -7,40 +7,32 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] === "POST"){
-        $username = trim($_POST["user"]);
+        $username = trim(strip_tags($_POST["user"]));
+        $username = str_replace(" ", "", $username);
         $password = $_POST["password"];
 
         if (empty($user) || empty($password)){
-            $mensaje = "<span>Error:</span> Los campos están vacios.";
+            exit();
         }
-        else{
+
+        if ((strlen($user) > 3) && (strlen($user) < 20)){
+            $password = password_hash($password, PASSWORD_BCRYPT);
+
             try{
-                $sql = $conn->prepare("SELECT * FROM usuarios WHERE username = ?");
-                $sql->execute([$username]);
-                $fetch = $sql->fetch(PDO::FETCH_ASSOC);
+                $conn = new PDO("mysql:host=$host:$puerto;dbname=$db", $user, $pass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                if ($fetch && password_verify($password, $fetch["password"])){
-                    // crear auth cookie
-                    $auth_cookie = bin2hex(random_bytes(128));
-                    $sql = $conn->prepare("UPDATE usuarios SET auth_cookie = ? WHERE id = ?");
-                    $sql->execute([$auth_cookie, $fetch["id"]]);
-
-                    $ult_act_activado = (int)$fetch["ult_act_activo"];
-
-                    setcookie("ult_act", $ult_act_activado, time() + (86400 * 30), "/"); 
-                    setcookie("auth", $auth_cookie, time() + (86400 * 30), "/"); 
-                    $_SESSION["cuenta_id"] = $fetch["id"];
-                    $_SESSION["cuenta_usuario"] = $fetch["username"];
-                    header("Location: index.php");
-                }
-                else{
-                    $mensaje = "<span>Error:</span> Usuario o contraseña incorrecta";
-                }
+                $sql = $conn->prepare("INSERT INTO usuarios(username, password, nickname) VALUES (?, ?, ?);");
+                $sql->execute([$username, $password, $username]);
+                header("Location: login.php?reg=1");
+                exit();
             }
             catch(PDOException $e){
-                $mensaje = "<span>Error:</span> Ha ocurrido un error, intente más tarde";
-                // mostrar error de manera más visual (base de datos caida)
+                $mensaje = "<span>Error:</span> El usuario ya existe";
             }
+        }
+        else{
+            $mensaje = "<span>Error:</span> El usuario es muy corto o muy largo";
         }
     }
 ?>
@@ -50,11 +42,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión</title>
+    <title>Registrarse</title>
     <link rel="stylesheet" href="styles/styles.css">
+    <script src="js/register/verify.js" defer></script>
     <link rel="shortcut icon" href="favicon.ico" />
-
-    <script src="js/login/sanidad.js" defer></script>
 </head>
 <body>
     <nav>
@@ -68,19 +59,21 @@
         </div>
     </nav>
     <div class="contenido-menu">
-        <form action="login.php" method="post" id="formulario-login">
-            <p id="texto-centrado">Iniciar sesión</p>
-            <input type="text" name="user" placeholder="Nombre de usuario" id="user-input" required>
-            <input type="password" name="password" placeholder="Contraseña" id="password-input" required>
-            <?php
-                if (isset($mensaje)){
-                    echo "<p id='formulario-mensaje2'>$mensaje</p>";
-                }
-                echo "<p id='formulario-mensaje' style='display: none;'></p>";
-            ?>
-            <input type="submit" value="Iniciar sesión">
+        <form action="register.php" method="post" id="formulario">
+            <p id="texto-centrado">Registrarse</p>
+            <input type="text" name="user" id="usernameF" placeholder="Nombre de usuario" required>
+            <input type="password" name="password" placeholder="Contraseña" id="contraseña" required>
+            <input type="password" name="verifyPassword" placeholder="Repetir contraseña" id="repetirContraseña" required>
+            <div id="register-mensaje">
+                <?php
+                    if (isset($mensaje)){
+                        echo "<p id='formulario-mensaje'>$mensaje</p>";
+                    }
+                ?>
+            </div>
+            <input type="submit" value="Registrarse">
         </form>
-        <p id="registrate">¿No tenés cuenta? Registrate <a href="register.php">acá</a></p>
+        <p id="registrate">¿Tenés cuenta? Iniciá sesión <a href="login.php">acá</a></p>
     </div>
 </body>
 </html>
