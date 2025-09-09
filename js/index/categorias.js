38ba05e45f13ea05388484355abbb4667485f1e9
cherry <tomas1234567891016@gmail.com>
2025-09-09 13:28:36 -0300

    Incluir &orden

--- js/index/categorias.js
+++ js/index/categorias.js
@@ -1,4 +1,5 @@
 const selector_categorias = document.getElementById("categoria-input-index");
+const selector_orden = document.getElementById("categoria-input-categoria");
 const tag_categoria = document.getElementById("input-tag-rojo-index")
 const input_busqueda = document.getElementById("input-busqueda");
 
@@ -11,9 +12,9 @@ selector_categorias.addEventListener("change", function() {
     categoria = valorSeleccionado;
     tag_categoria.textContent = "/" + valorSeleccionado + "/"; 
     if (input_busqueda.value.length > 0){
-        window.location.replace("?categoria=" + categoria + "&q=" + input_busqueda.value);
+        window.location.replace("?categoria=" + categoria + "&q=" + input_busqueda.value + "&orden=" + selector_orden.value);
     }
     else{
-        window.location.replace("?categoria=" + categoria);
+        window.location.replace("?categoria=" + categoria + "&orden=" + selector_orden.value);
     }
 });
